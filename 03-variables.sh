3a52f7f7adc957cfd18a0889723023e9276b8ed5
nanda <baskara.sadananda@gmail.com>
2024-04-16 00:45:31 +0530

    adding variables files

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "passing variables from the command line"
+
+P1=$1
+P2=$2
+
+echo "$P1 :: Hi $P2 gm"
+echo "$P2 :: Hi $P1 vgm"
+echo "$P1 :: How are you $P2"
+echo "$P2 :: I am doing good $P1.Thank you "
