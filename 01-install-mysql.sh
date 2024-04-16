b410238f29285ebbd3389f430262a07f32d90012
nanda <baskara.sadananda@gmail.com>
2024-04-16 21:59:52 +0530

    conditions

--- 01-install-mysql.sh
+++ 01-install-mysql.sh
@@ -18,3 +18,13 @@ then
 else
 	echo "mysql installation is success"
 fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then 
+	echo "git installation is failed"
+else
+	echo "git installation is success"
+
+fi
