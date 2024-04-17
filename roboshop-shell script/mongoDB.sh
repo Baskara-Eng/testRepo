53cb5c9761d51bf5cda9d05bdccbce9a564dde6a
nanda <baskara.sadananda@gmail.com>
2024-04-17 23:06:58 +0530

    roboshop shell scipt

--- roboshop-shell script/mongoDB.sh
+++ roboshop-shell script/mongoDB.sh
@@ -40,7 +40,7 @@ systemctl start mongod
 
 VALIDATE $? "staring mongodb"
 
-sed -i 's /127.0.0.0/0.0.0.0/g' /etc/mongod.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOGFILE
 
 systemctl restart mongod &>>$LOGFILE
 
