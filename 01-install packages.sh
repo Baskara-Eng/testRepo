00e1aad3985ed435616c6eb439ae3b15d925e142
nanda <baskara.sadananda@gmail.com>
2024-04-17 21:26:44 +0530

    conditions

---
+++ 01-install packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-H-%M-%S)
+LOG="/tmp/$0-$TIMESTAMP.log"
+
+if [ $ID -ne 0 ]
+then
+	echo "please run command with root user"
+	exit 1
+else
+	echo "you are root user"
+fi
+
+VALIDATE(){
+	if [ $1 -ne 0 ]
+	then
+		echo "$2 installtion is failed"
+	else
+		echo "$2 installtion is success"
+	fi
+}
+
+yum install mysql -y 
+
+VALIDATE $? "mysql"
+
+yum install git -y
+
+VALIDATE $? "git"
