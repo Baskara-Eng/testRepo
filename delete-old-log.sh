b4c375e14e2c594e3a1553a9d491ae29252ad39d
nanda <baskara.sadananda@gmail.com>
2024-04-19 21:27:51 +0530

    create delete log file

--- delete-old-log.sh
+++ delete-old-log.sh
@@ -9,10 +9,10 @@ then
 	echo "Source directory is not exist"
 fi 
 
-FILES_TO_DELETE=$(find $SOURCE_DIR f -mtime 14 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime 14 -name "*.log")
 
 while IFS = read -r line
 do
 	echo "Deleting line $line"
 	rm -rf $line
-done <<< FILES_TO_DELETE
+done <<< $FILES_TO_DELETE
