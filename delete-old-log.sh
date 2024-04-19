552a46343a95777ce08d0cea7db82a5bf1a500c9
nanda <baskara.sadananda@gmail.com>
2024-04-19 21:23:40 +0530

    create delete log file

---
+++ delete-old-log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+SOURCE_DIR=/tmp/shellscript.logs
+
+#check directory is exist or not 
+if [ !d SOURCE_DIR=]
+then
+	echo "Source directory is not exist"
+fi 
+
+FILES_TO_DELETE=$(find $SOURCE_DIR f -mtime 14 -name "*.log")
+
+while IFS = read -r line
+do
+	echo "Deleting line $line"
+	rm -rf $line
+done <<< FILES_TO_DELETE
